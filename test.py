24f82687666b886de80faea156b3f49bfbed10c0
aboabdo123 <135479908+aboabdo123@users.noreply.github.com>
2023-11-18 21:11:08 -0800

    test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+import unittest
+from flask
+import app, cart, get_items
+
+class VendingMachineTest(unittest.TestCase):
+
+  def setUp(self):
+    self.app = app.test_client()
+
+def test_item_selection(self):
+
+def test_cart_chekout(self):
+
+def test_card_payment(self):
+
+def test_cash_payment(self):
+
+def test_customer_interface(self):
+
+def test_item_dispense(self):
+
+def test_restocker_login(self):
+
+def test_restocker_instructions(self):
+
+def test_item_scanning(self):
+
+def test_inventory_update(self):
+
+def test_update_corporate_database(self):
+
+def test_management_tool_login(self):
+       
+def test_management_tool_inventory_view(self):
+        
+def test_management_tool_analytics(self):
+       
+def test_management_tool_instruction_update(self):
+       
+def test_management_tool_vm_errors(self):
+        
+
+if __name__ == '__app__':
+    unittest.app()
